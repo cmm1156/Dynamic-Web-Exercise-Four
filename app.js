e5b3da44842b9584a0207399942705d37b63d37f
Christopher Mancini <cmm1156@nyu.edu>
2020-11-10 14:42:58 -0500

    Connected Firebase

--- app.js
+++ app.js
@@ -2,8 +2,25 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
 
+// Configuration Values for Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyCEm1eiR_jGZqYNY8LvyrhrL50DVd4pbYo",
+  authDomain: "exercise-four-500f8.firebaseapp.com",
+  databaseURL: "https://exercise-four-500f8.firebaseio.com",
+  projectId: "exercise-four-500f8",
+  storageBucket: "exercise-four-500f8.appspot.com",
+  messagingSenderId: "508889392789",
+  appId: "1:508889392789:web:c960ce85af8c49873bdd9c",
+};
+
+// Firebase
+const firebase = require("firebase");
+firebase.initializeApp(firebaseConfig);
+
 // Routes Import
 const indexRoute = require("./routes/index.js");
+const postRoute = require("./routes/post.js");
+const createRoute = require("./routes/createBlogpost.js");
 // Routes
 app.use("/", indexRoute);
 
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,32 @@
+// Show all blogposts
 const express = require("express");
-
 const router = express.Router();
 
-router.get("/", (req, res) => res.send("Exercise Four"));
+// Require Firebase
+const firebase = require("firebase");
+// Initialize Firestore Database
+const db = firebase.firestore();
+// Reference a specific collection
+const blogposts = db.collection("blogposts");
+
+router.get("/", (req, res) => {
+  // Inside of this arrow function, we can do aanything we along as somethign is returned at the end
+  const blogpostsArray = [];
+
+  blogposts
+    .get()
+    .then((querySnapshot) => {
+      // console.log("querySnapshot", querySnapshot);
+      // Loop through query snapshot and push to array
+      querySnapshot.forEach((doc) => {
+        blogpostsArray.push(doc.data());
+      });
+      return res.send(blogpostsArray);
+    })
+    .catch(function (e) {
+      console.warn("error", e);
+      return res.send(error);
+    });
+});
 
 module.exports = router;
