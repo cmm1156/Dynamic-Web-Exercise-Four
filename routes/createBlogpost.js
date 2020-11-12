ced8b5f0f7f5c22e945d40fd61fc60444e6e408a
Christopher Mancini <cmm1156@nyu.edu>
2020-11-12 15:02:31 -0500

    added blogpost creation forms

--- routes/createBlogpost.js
+++ routes/createBlogpost.js
@@ -1,8 +1,23 @@
 // Show all blogposts
 const express = require("express");
-
 const router = express.Router();
 
-router.get("/", (req, res) => res.send("Create a Blogpost"));
+// Require Firebase
+const firebase = require("firebase");
+// Initialize Firestore Database
+const db = firebase.firestore();
+// Reference a specific collection
+const blogposts = db.collection("blogposts");
+
+const form = `
+<form>
+  <input type="text" name="title" placeholder="Title of Post" />
+  <input type="text" name="text" placeholder="Text of Post" />
+  <input type="text" name="author" placeholder="Author" />
+  <button type="submit">Submit Post</button>
+</form>
+`;
+
+router.get("/", (req, res) => res.send(form));
 
 module.exports = router;
